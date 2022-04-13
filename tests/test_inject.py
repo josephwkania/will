496667c33fcfa3544b4346df37a33c117df229a1
josephwkania <60994292+josephwkania@users.noreply.github.com>
2022-04-13 03:15:35 -0400

    Error handling tests for inject

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -2,6 +2,10 @@
 """
 Test Pulse injection routines.
 """
+
+# Can't use inits with pytest, this error is unavoidable
+# pylint: disable=W0201
+
 # pylint: disable=redefined-outer-name
 # The pytest.fixture needs to be redefined
 
@@ -64,37 +68,78 @@ def create_fil(tmpdir_factory):
     return out_name, dynamic
 
 
-def test_inject_constant(create_fil):
+class TestInjectConstant:
     """
-    Inject a constant pulse into the fake fil
+    Inject constant into file
     """
-    simple_pulse = create.SimpleGaussPulse(
-        0.01,
-        dm=DM,
-        tau=5,
-        chan_freqs=CHAN_FREQS,
-        sigma_freq=100,
-        center_freq=1050,
-        tsamp=TSAMP,
-        spectral_index_alpha=1,
-        nscint=1,
-        phi=1,
-    )
-    pulse = simple_pulse.sample_pulse(int(4e5))
 
-    yr_obj = Your(create_fil[0])
-    pulse = inject.inject_constant_into_file(  # noqa: max-complexity:30
-        yr_obj,
-        pulse,
-        start=0,
-        period=100,
-        gulp=NUM_SAMP,
-        out_fil=None,
-        clip_powers=True,
-    )
+    def setup_class(self):
+        """
+        Make the pulse
+        """
+        simple_pulse = create.SimpleGaussPulse(
+            0.01,
+            dm=DM,
+            tau=5,
+            chan_freqs=CHAN_FREQS,
+            sigma_freq=100,
+            center_freq=1050,
+            tsamp=TSAMP,
+            spectral_index_alpha=1,
+            nscint=1,
+            phi=1,
+        )
+        self.pulse = simple_pulse.sample_pulse(int(4e5))
 
-    assert pulse.dtype == yr_obj.your_header.dtype
-    assert pulse.shape == (NUM_SAMP, NCHANS)
+    def test_inject_constant(self, create_fil):
+        """
+        Inject a constant pulse into the fake fil
+        """
+        yr_obj = Your(create_fil[0])
+        inj_pulse = inject.inject_constant_into_file(  # noqa: max-complexity:30
+            yr_obj,
+            self.pulse,
+            start=0,
+            period=100,
+            gulp=NUM_SAMP,
+            out_fil=None,
+            clip_powers=True,
+        )
+
+        assert inj_pulse.dtype == yr_obj.your_header.dtype
+        assert inj_pulse.shape == (NUM_SAMP, NCHANS)
+
+    def test_chan_error(self, create_fil):
+        """
+        If this pulse is the wrong size raise an error
+        """
+        yr_obj = Your(create_fil[0])
+        with pytest.raises(RuntimeError):
+            inject.inject_constant_into_file(  # noqa: max-complexity:30
+                yr_obj,
+                self.pulse[:, :10],
+                start=0,
+                period=100,
+                gulp=NUM_SAMP,
+                out_fil=None,
+                clip_powers=True,
+            )
+
+    def test_gulp_error(self, create_fil):
+        """
+        If this pulse is the wrong size raise an error
+        """
+        yr_obj = Your(create_fil[0])
+        with pytest.raises(RuntimeError):
+            inject.inject_constant_into_file(  # noqa: max-complexity:30
+                yr_obj,
+                self.pulse[:, :10],
+                start=0,
+                period=100,
+                gulp=1,
+                out_fil=None,
+                clip_powers=True,
+            )
 
 
 def test_num_pulses():
@@ -107,40 +152,66 @@ def test_num_pulses():
     assert isinstance(npulse, np.integer)
 
 
-def test_inject_distro(create_fil):
+class TestInjectDistro:
     """
-    Inject a distrbution into the file
+    Test injecting a distributution
     """
-    simple_pulse = create.SimpleGaussPulse(
-        0.01,
-        dm=DM,
-        tau=5,
-        chan_freqs=CHAN_FREQS,
-        sigma_freq=100,
-        center_freq=1050,
-        tsamp=TSAMP,
-        spectral_index_alpha=1,
-        nscint=1,
-        phi=1,
-    )
 
-    period = 100
-    npulse = inject.num_pulses(period, NUM_SAMP)
-    powers = 10e2 + 10 * np.random.normal(size=npulse)
-    np.around(powers, out=powers)
-    powers = powers.astype(int)
+    def setup_class(self):
+        """
+        Create the pulse
+        """
+        self.simple_pulse = create.SimpleGaussPulse(
+            0.01,
+            dm=DM,
+            tau=5,
+            chan_freqs=CHAN_FREQS,
+            sigma_freq=100,
+            center_freq=1050,
+            tsamp=TSAMP,
+            spectral_index_alpha=1,
+            nscint=1,
+            phi=1,
+        )
 
-    yr_obj = Your(create_fil[0])
-    pulse_distro = inject.inject_distribution_into_file(
-        yr_obj,
-        simple_pulse,
-        pulse_counts=powers,
-        start=0,
-        period=period,
-        gulp=NUM_SAMP,
-        out_fil=None,
-        clip_powers=True,
-    )
+        self.period = 100
+        npulse = inject.num_pulses(self.period, NUM_SAMP)
+        powers = 10e2 + 10 * np.random.normal(size=npulse)
+        np.around(powers, out=powers)
+        self.powers = powers.astype(int)
 
-    assert pulse_distro.dtype == yr_obj.your_header.dtype
-    assert pulse_distro.shape == (NUM_SAMP, NCHANS)
+    def test_inject_distro(self, create_fil):
+        """
+        Inject a distrbution into the file
+        """
+        yr_obj = Your(create_fil[0])
+        pulse_distro = inject.inject_distribution_into_file(
+            yr_obj,
+            self.simple_pulse,
+            pulse_counts=self.powers,
+            start=0,
+            period=self.period,
+            gulp=NUM_SAMP,
+            out_fil=None,
+            clip_powers=True,
+        )
+
+        assert pulse_distro.dtype == yr_obj.your_header.dtype
+        assert pulse_distro.shape == (NUM_SAMP, NCHANS)
+
+    def test_gulp_error(self, create_fil):
+        """
+        If the gulp is too small, raise an error
+        """
+        yr_obj = Your(create_fil[0])
+        with pytest.raises(RuntimeError):
+            inject.inject_distribution_into_file(
+                yr_obj,
+                self.simple_pulse,
+                pulse_counts=self.powers,
+                start=0,
+                period=self.period,
+                gulp=1,
+                out_fil=None,
+                clip_powers=True,
+            )
